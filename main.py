fcba70c19b5cc605b566abc8c5349edb52b74574
MichaelIcaza <Vannak139@gmail.com>
2019-10-11 10:39:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 """
-This file runs the main experiment. 
-Note, this procedure returns a tensorflow warning about large droprate. This is not an issue, we intend to use an abnormally large dropout rate
-
-
+This file generates sample images, no CV
 """
 import os
 import time
 import pickle as pkl
 import numpy as np
 import pandas as pd
-from model_functions import DataGenerator, PlotLoss
 from model import build_model
+from model_functions import DataGenerator, PlotLoss, PlotHist, roc_callback, Plot_Predictions
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.model_selection  import StratifiedKFold
 
-data_dir = "E:/Wu/numpy_data"
+data_dir = "../NumpyData_1x"
 trial_dir = time.strftime("%Y-%m-%d_%H-%M/")
 os.mkdir(trial_dir)#create folder for weights, labeled by date-time
 
@@ -43,7 +40,7 @@ patient_list = np.asarray(patient_list)
 target_list = np.asarray(target_list)
 
 #======================== Split patients into folds
-skfold = StratifiedKFold(n_splits=5, random_state=1)#sk-learn's staratified K-fold splits while keeping fold balanced about the target
+skfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=128)#sk-learn's staratified K-fold splits while keeping fold balanced about the target
 folds = list(skfold.split(patient_list, target_list))
 
 #save an early copy of the fold data, for analysis mostly
@@ -59,84 +56,61 @@ fold_df = pd.DataFrame({"Patient_ID":fold_arr[:,0], "Fold":fold_arr[:,1]})
 fold_df.to_csv(trial_dir+"fold_data.csv", index=False)#if index is true, a 0th column is added that just has row number
         
 
-
 #=================================Begin the experiment
-for droprate in [0.25, 0.85, 0, 0.5, 0.75, 0.9]:#itterate over the drop rates
-    experiment_log=[]#this holds a list of the other logs, collected for every different drop rate
-    trial_preds = {}#this holds img_name:prediction, added to over each fold
-    for i in range(5):#itterate over the folds
-        hist_log = []#training log from keras
-        pred_log = []#predictions at end of training
-        sample_log = []#names of the samples ([train],[val]) format
 
-        #we have the patient-level folds, we get the iamge-level samples
-        train_id_ix = list(folds[i][0])
-        train_ids = [patient_list[ix] for ix in train_id_ix]
-        train_ix = [name[:2] in train_ids for name in img_names]
-        val_id_ix = list(folds[i][1])
-        val_ids = [patient_list[ix] for ix in val_id_ix]
-        val_ix = [name[:2] in val_ids for name in img_names]
-        
-        #split the train/val data by image name
-        train_loc = img_loc[train_ix]#list of image names in train set
-        train_tar = img_tar[train_ix]
-        train_len = len(train_loc)
-        val_loc = img_loc[val_ix]#list of image names in validation set
-        val_tar = img_tar[val_ix]
-        val_len = len(val_loc)
-        
-        #open all of the train-fold images
-        x_train = np.asarray([np.load(file) for file in train_loc])
-        x_val = np.asarray([np.load(file) for file in val_loc])
-        
-        train_generator = DataGenerator(x_train, train_tar, 1, shuffle=True, augment=True, weighted=True)
-        val_generator = DataGenerator(x_val, val_tar, 1, shuffle=False, augment=False, weighted=False)
-        
-        #this is to account for the batch size
-        
-        sizes = [16, 32, 32, 64, 16]
-        lr = 1e-4
-        optimizer = "Adam"
-    
-        model = build_model(out_type="class", sizes=sizes, droprate=droprate, optimizer=optimizer, lr=lr, grad_accum=1)#the model trains in "class mode", and can output a "heat_map" in later analysis
-        weight_name = trial_dir+str(int(droprate*100))+"_"+str(i+1)+"fold_weights.hdf5"
-        #model.summary()#this will print out model details including layer sizes and parameter count ~100K
-        plot_loss = PlotLoss(freq=10)
-        #earlystop = EarlyStopping(monitor='loss', min_delta=0, patience=5, verbose=0)#note: normally validation loss would be selected, we have noisy validation loss so we select this method
-        checkpoint = ModelCheckpoint(weight_name, monitor='loss', verbose=0, save_best_only=True, save_weights_only=True, mode='auto', period=1)#we do not check the validation loss, its technically our "test set", its invisible during training        
-        hist = model.fit_generator(train_generator, steps_per_epoch=train_len, epochs=50,
-                            validation_data=val_generator, validation_steps=val_len,
-                            callbacks=[plot_loss, checkpoint],
-                            max_queue_size=128, workers=16)
-        model.load_weights(weight_name)#reload the best weights, according to monitored training loss
-        preds = model.predict_generator(val_generator, val_len)
-        
-        #append to the fold-log (each of these should hold 5 items)
-        sample_log.append((train_loc, val_loc))
-        hist_log.append(hist.history)
-        pred_log.append(preds)
-        
-        for n, pred in enumerate(preds):#for each pred, get the loc, extract the name, create dict name>pred
-            img_name = val_loc[n].split('/')[-1][:-4]
-            trial_preds[img_name] = pred[0]#we do this over every fold and eventually get a (validation/test) prediction for every image, the [0] makes it return a float rather than a [0.2413] number
-            
-        del model#to reset weights
-    experiment_log.append([droprate, sample_log, hist_log, pred_log])#append to a list, including the drop rate. 
-    #a complete 5-fold validation has been done
-    #save as a pickle for later analysis, for every trial
-    str_drop=str(int(droprate*100))
-    with open(trial_dir+str_drop+'.pkl', 'wb') as f:
-        pkl.dump(experiment_log, f)
-    
-    #also write a csv for ongoing-analysis while experiment is running
-    #columns: File name, patient_id, Fold, Target, Pred
-    id_data = [name[:2] for name in img_names]
-    fold_data = [patient_fold_dict[int(id_)] for id_ in id_data]
-    pred_data = [trial_preds[name] for name in img_names]
-    trial_df = pd.DataFrame({"File_Name":img_names, "Patient_ID":id_data, "Fold":fold_data, "Target":img_tar, "Predicted":pred_data})
-    trial_df.to_csv(trial_dir+str_drop+"_predictions.csv", index=False)
-    
-    
-            
+train_id_ix = list(folds[1][0])
+train_ids = [patient_list[ix] for ix in train_id_ix]
+train_ix = [name[:2] in train_ids for name in img_names]
+train_loc = img_loc[train_ix]#list of image names in train set
+train_tar = img_tar[train_ix]
+train_len = len(train_loc)
 
 
+val_id_ix = list(folds[1][1])
+val_ids = [patient_list[ix] for ix in val_id_ix]
+val_ix = [name[:2] in val_ids for name in img_names]
+val_loc = img_loc[val_ix]#list of image names in validation set
+val_tar = img_tar[val_ix]
+val_len = len(val_loc)
+
+#open all of the train-fold images
+x_train = np.asarray([np.load(file) for file in train_loc])
+x_val = np.asarray([np.load(file) for file in val_loc])
+    
+train_generator = DataGenerator(x_train, train_tar, 1, shuffle=True, augment=True, weighted=False)
+val_generator = DataGenerator(x_val, val_tar, 1, shuffle=False, augment=False, weighted=False)
+
+#fixed parameters
+sizes = [16, 32, 32, 64, 16]
+lr = 1e-5
+optimizer = "Nadam"
+droprate=0.25
+
+weight_name = trial_dir+str(int(droprate*100))+"_"+str(i+1)+"fold_weights.hdf5"
+
+model = build_model(out_type="class", sizes=sizes, droprate=droprate, optimizer=optimizer, lr=lr, grad_accum=1)#the model trains in "class mode", and can output a "heat_map" in later analysis
+map_model = build_model(out_type="heat_map", sizes=sizes, droprate=droprate, optimizer=optimizer, lr=lr, grad_accum=1)
+
+#model.summary()#this will print out model details including layer sizes and parameter count ~100K
+
+#load the same initial random weights for each trial
+
+if os.path.exists(trial_dir+"initial_weights.hdf5"):#if the initial weights exist, load. otherwise, save
+    model.load_weights(trial_dir+"initial_weights.hdf5")
+else:
+    model.save_weights(trial_dir+"initial_weights.hdf5")
+
+plot_loss = PlotLoss(freq=10)#plots training metrics
+plot_hist = PlotHist(map_model, val_generator, val_len, val_tar,10)#plots histogram of activations
+plot_preds=Plot_Predictions(x_val, val_tar, [5,15,23,2,9,18])#plot predictions with bounding boxes
+aucroc_callback = roc_callback((x_train, train_tar),(x_val, val_tar))#print out AUCROC metric
+earlystop = EarlyStopping(monitor='loss', min_delta=0, patience=25, verbose=0)#note: normally validation loss would be selected, we have noisy validation loss so we select this method
+
+checkpoint = ModelCheckpoint(weight_name, monitor='loss', verbose=0, save_best_only=True, save_weights_only=True, mode='auto', period=1)#we do not check the validation loss, its technically our "test set", its invisible during training        
+hist = model.fit_generator(train_generator, steps_per_epoch=train_len, epochs=300,
+                    validation_data=val_generator, validation_steps=val_len,
+                    callbacks=[plot_loss, checkpoint, plot_hist, aucroc_callback, earlystop, plot_preds],
+                    max_queue_size=128, workers=16)
+    
+   
+model.load_weights(weight_name)#reload the best weights, according to monitored training loss
